34de4c5938466c39f3f1b846ee4d6f899986cad4
MaKow19 <kowol.maik@gmail.com>
2024-08-04 17:58:18 +0200

    stlying

--- src/Pages/Canvas.tsx
+++ src/Pages/Canvas.tsx
@@ -15,36 +15,36 @@ import InterpreterModeIcon from '@mui/icons-material/InterpreterMode';
 const Layout = () => {
     return (
         
-            <Stack width={'1800px'} display={'flex'} direction={'row'} justifyContent={'center'} spacing={2} useFlexGap flexWrap="wrap">
+            <Stack width={'2px'} display={'flex'} direction={'row'} justifyContent={'center'} spacing={2} useFlexGap flexWrap="wrap">
                 <Card>
-                    <Stack bgcolor={'#ffffff'} sx={{ minWidth: 350, minHeight: 500, overflow: 'auto', width: 200, height: 424, border: 'solid'}}>
+                    <Stack bgcolor={'#ffffff'} sx={{ minWidth: 300, minHeight: 500, overflow: 'auto', width: 190, height: 424, border: 'solid'}}>
                         <Cell index={0} title={'Festlegung der Rahmenbendingen'} ></Cell>
                     </Stack>
                 </Card>
                 <Card>
-                    <Stack bgcolor={'#ffffff'} sx={{ minWidth: 350, minHeight: 500, overflow: 'auto', width: 200, height: 424, border: 'solid'}}>
+                    <Stack bgcolor={'#ffffff'} sx={{ minWidth: 300, minHeight: 500, overflow: 'auto', width: 190, height: 424, border: 'solid'}}>
                         <Cell index={1} title={'Hinweise erfassen'} numberIcon={<LooksOneIcon/>} titleIcon={<LiveHelpIcon/>}></Cell>
                     </Stack>
                 </Card>
                 <Card>
-                    <Stack bgcolor={'#ffffff'} sx={{ minWidth: 350, minHeight: 500, overflow: 'auto', width: 200, height: 424, border: 'solid'}}>
+                    <Stack bgcolor={'#ffffff'} sx={{ minWidth: 300, minHeight: 500, overflow: 'auto', width: 190, height: 424, border: 'solid'}}>
                         <Cell index={2} title={'Festlegung des Hinweises'} numberIcon={<LooksTwoIcon/>} titleIcon={<FeedbackIcon/>}></Cell>
                     </Stack>
                 </Card>
                 <Card>
-                    <Stack bgcolor={'#ffffff'} sx={{ minWidth: 350, minHeight: 500, overflow: 'auto', width: 200, height: 424, border: 'solid'}}>
+                    <Stack bgcolor={'#ffffff'} sx={{ minWidth: 300, minHeight: 500, overflow: 'auto', width: 190, height: 424, border: 'solid'}}>
                         <Cell index={3} title={'Teamreflektion'} numberIcon={<Looks3Icon/>} titleIcon={<GroupsIcon/>}></Cell>
                     </Stack>
                 </Card>
                 
                 <Stack>
                     <Card>
-                        <Stack bgcolor={'#ffffff'} sx={{ minWidth: 350, maxHeight: 239, overflow: 'auto', width: 200, height: 424, border: 'solid'}}>
+                        <Stack bgcolor={'#ffffff'} sx={{ minWidth: 300, maxHeight: 239, overflow: 'auto', width: 190, height: 424, border: 'solid'}}>
                             <Cell index={4} title={'Proaktiv Hindernisse erkennen'} numberIcon={<Looks4Icon/>} titleIcon={<LiveHelpIcon/>}></Cell>
                         </Stack>
                     </Card>
                     <Card sx={{mt: 2}}>
-                        <Stack bgcolor={'#ffffff'} sx={{minWidth: 350, maxHeight: 239, overflow: 'auto', width: 200, height: 424, border: 'solid'}}>
+                        <Stack bgcolor={'#ffffff'} sx={{minWidth: 300, maxHeight: 239, overflow: 'auto', width: 190, height: 424, border: 'solid'}}>
                             <Cell index={5} title={'Gemeinsames Verständnis der Beteiligten'} numberIcon={<Looks5Icon/>} titleIcon={<InterpreterModeIcon/>}></Cell>
                         </Stack>
                     </Card>
